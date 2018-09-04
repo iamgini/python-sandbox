fc2010f18489229f9a14893cd918c7d9ae0735c6
Gineesh Mada Pparambath <net.gini@gmail.com>
2018-09-04 13:50:06 +0800

    6-47

--- python-6-47-level2-problems.py
+++ python-6-47-level2-problems.py
@@ -8,3 +8,24 @@ def has_33(nums):
 print (has_33([1,3,3]))
 
 #paper doll - repeat each characters 3 times in a string
+def paper_doll(text):
+    result = ""
+    for char in text:
+        result += char*3
+    return result
+
+print(paper_doll("Hello"))
+
+#blackjack - 
+def blackjack(a,b,c):
+    if sum([a,b,c]) <= 21:
+        return sum([a,b,c])
+    elif 11 in [a,b,c] and sum([a,b,c])-10 <=21:
+        return sum([a,b,c])-10
+    else:
+        return "BUST"
+
+print(blackjack(5,10,11))
+
+
+
