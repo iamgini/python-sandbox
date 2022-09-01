e1833db467333faac2cef6721e8b9df54a93764d
ginigangadgaran <net.gini@gmail.com>
2022-09-01 16:15:06 +0800

    update

--- 101-car-manager.py
+++ 101-car-manager.py
@@ -50,11 +50,90 @@ def add_car():
             # db_file = open(database_file, 'r')
             # print(db_file.read())
             # db_file.close()
+            #dict2 = {car_model_name.replace(" ","_"):{"model": car_model_name, "year": car_make_year}}
+
+
+            #dict2 = { "car_list": { "brand1": [ { "model_name": "hello", "year": 2022},{"model_name": "hai", "year": 2021 }]}}
+            car_list = { car_model_name.replace(" ","_"): { "model_name": car_model_name.replace(" ","_"), "year": int(car_make_year)}}
+            # dict2 = { "car_list": { car_brand_name_selected: [ { "model_name": car_model_name.replace(" ","_"), "year": int(car_make_year)}]}}
+            print(car_list)
+
+            if 'brand1' in car_list:
+                print("brand1")
+            else:
+                car_list.update({'brand1':{}})
+
+            if 'model1' in car_list:
+                print("model1")
+            else:
+                car_list['brand1'].update({'model1':{}})
+            print(car_list)
+            
+
+
+            # dict3 = { "car_list": { "newbrand": [ { "model_name": car_model_name.replace(" ","_"), "year": int(car_make_year)}]}}
+            # dict2 = { "car_list": { car_brand_name_selected: [ { "model_name": car_model_name.replace(" ","_"), "year": int(car_make_year)}]}}
+            #car_list[car_brand_name_selected]["model_name"] = car_model_name.replace(" ","_")
+            #car_list[car_brand_name_selected]["year"] = car_make_year
+            
+            # dict2({ "car_list": { "newbrand": [ { "model_name": car_model_name.replace(" ","_"), "year": int(car_make_year)}]}})
+            new_value = { "brand1": { "model2": { "model_name": "hello", "year": 2022 }}}
+            # { "brand1": [{ "model_name": "hello", "year": 2022 }]}
+            print(new_value)
+
+            car_list.update(new_value)
+
+            print(car_list)
+
+
+            new_value = { "brand1": { "model3": { "model_name": "hello2", "year": 2022 }}}
+            # { "brand1": [{ "model_name": "hello", "year": 2022 }]}
+            print(new_value)
+            
+            #car_list1 = car_list | new_value
+            car_list.update(new_value)
+
+            print(car_list)
+
+            # Read file
+            db_file = open(database_file, 'r')
+            print(db_file.read())
+            db_file.close()
+
 
 
     else:
         print("Input is out of range, please re-enter: ")
         add_car()
+
+
+    
+      # what the user wants
+    # while True:
+    #     item = input("Enter the items you wish to add into the shopping cart: ").title()  # what the user wants
+    #     if item not in price_list:  # check for correct input
+    #             print("The item is not in the list")
+    #     else:
+    #         quantity = float(input("How many would you like?: "))  # how many user want
+    #         print("{:.0f} of {} has been added to the cart!".format(quantity, item))
+    #         item_dict[item] = quantity
+    #         more_items = input('Would you like to add more? (y/n): ').lower()
+    #         if more_items == 'y':
+    #             item = input("Enter the items you wish to add into the shopping cart: ").title()  # what the user wants
+    #             quantity = float(input("How many would you like?: "))  # how many user want
+    #             print(start_menu())
+    #              # check if same item is inside cart
+    #             if item in item_dict:
+    #                 print('Another {:.0f} of {} has been added to the cart!'.format(quantity, item))
+    #             else:
+    #                 print("{:.0f} of {} has been added to the cart!".format(quantity, item))
+    #                 pass
+    #         else:
+    #             print(start_menu())
+
+# new_blog = open(blog_git_location + article_published_date_for_file + '-' + article_new_blog_file + '.md', "w")
+#       new_blog.write(templated_output)
+#       new_blog.close()
 
 # ------- add car ------------------
 def view_cars():
